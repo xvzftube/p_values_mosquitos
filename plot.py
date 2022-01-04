60f8bb081deee21e7a149a99f1249ba0f10a32b7
xvzf <70577318+xvzftube@users.noreply.github.com>
2022-01-03 21:10:25 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ from plotnine import theme_set, ggplot, geom_histogram, xlab, ggtitle, geom_vlin
 palette = ['#ee1d52','#f2d803', '#69c9d0']
 
 # read csv
-df = pd.read_csv("./code/df_diff.csv")
+df = pd.read_csv("./df_diff.csv")
 
 # quick plot of time series
 theme_set(theme_538)
